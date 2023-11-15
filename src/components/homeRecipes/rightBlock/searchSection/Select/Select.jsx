d56cf83bcee06eb2e170b10bec56ef575ce754df
Margo-Katsan <123902159+Margo-Katsan@users.noreply.github.com>
2023-11-15 14:44:05 +0100

    s

--- src/components/homeRecipes/rightBlock/searchSection/Select/Select.jsx
+++ src/components/homeRecipes/rightBlock/searchSection/Select/Select.jsx
@@ -1,31 +1,25 @@
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
+import { useSearchParams } from 'react-router-dom';
 import { HiChevronDown, HiChevronUp } from 'react-icons/hi';
-import { useResetFilter } from 'hooks/useResetFilter';
-import svg from 'images/sprite.svg'
 import css from "./Select.module.css"
 
 
-export const Select = ({ options, placeholder, onSelect, onReset, filterToReset }) => {
-  const { selectedOption, isSelect, handleResetSelectedOption, handleChangeSelectedOption } = useResetFilter(placeholder);
-  
-  const handleReset = () => {
-    onReset(filterToReset);
-    handleResetSelectedOption({value: '', label: placeholder})
-  }
+export const Select = ({ options, placeholder, onSelect, filterToReset }) => {
+  const [searchParams] = useSearchParams();
 
   return (
     <div className={css.container}>
       <Dropdown
         options={options}
         placeholder={placeholder}
-        value={selectedOption}
+        value={searchParams.get(filterToReset) ?? ''}
         className={css.wrapper}
         controlClassName={css.control}
         placeholderClassName={css.placeholder}
         menuClassName={css.menu}
         
-        onChange={evt => { onSelect(evt); handleChangeSelectedOption(evt) }}
+        onChange={evt => { onSelect(evt); }}
         
         arrowClosed={
           <span className="arrow-open">
@@ -40,13 +34,6 @@ export const Select = ({ options, placeholder, onSelect, onReset, filterToReset 
       
       />
 
-      {isSelect && (
-        <span className={css.close}>
-          <svg width='16' height='16' fill='rgba(5, 5, 5, 0.5)' onClick={handleReset}>
-            <use href={`${svg}#search-close`}></use>
-          </svg>
-        </span>
-      )}
     </div>
   )
 }
--- src/components/homeRecipes/rightBlock/searchSection/TitleLabel/TitleLabel.jsx
+++ src/components/homeRecipes/rightBlock/searchSection/TitleLabel/TitleLabel.jsx
@@ -1,11 +1,12 @@
 import _ from 'lodash';
+import { useSearchParams } from 'react-router-dom';
 import { useExistingSearchParams } from "hooks/useExistingSearchParams";
 import { LabelName } from "../LabelName/LabelName";
 import svg from "images/sprite.svg";
 import css from "./TitleLabel.module.css"
 
 export const TitleLabel = () => {
-
+  const [searchParams] = useSearchParams();
   const { updatingSearchParams } = useExistingSearchParams();
   const handleInputText = _.debounce(
     e => {
@@ -21,17 +22,18 @@ export const TitleLabel = () => {
       <LabelName name={"Search"} />
       <span className={css.wrapper}>
         
-      <input
-        type="text"
+        <input
+          type="text"
           id="search-key"
           className={css.input}
           name="search-key"
+          defaultValue={searchParams.get('title') ?? ''}
           placeholder="Enter text"
           onInput={handleInputText}
         />
         <svg className={`icon ${css.icon}`} width="18px" height="18px">
-        <use href={`${svg}#search`}></use>
-      </svg>
+          <use href={`${svg}#search`}></use>
+        </svg>
       </span>
     </label>
   )
